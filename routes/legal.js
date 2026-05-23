5111a809aa565bc85134dc399183235168b91b0a
Samuel Olabode <solabode499@gmail.com>
2026-05-22 20:46:35 -0600

    Add 'Contact Us' route and update footer link on landing page

--- routes/legal.js
+++ routes/legal.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 router.get('/privacy-policy', (req, res)=>{
     res.render('legal/privacyPolicy', {title : 'Privacy Policy'})
+});
+
+router.get('/contact-us', (req, res)=>{
+    res.render('legal/contactUs', {title : 'Contact Us'})
 })
 
 module.exports = router;
